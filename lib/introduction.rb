a828e8a607e0b5bb0a13f8574b7f0ace82122d80
Tom White <eugene.white@gmail.com>
2019-04-29 04:55:01 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,7 @@
 def introduction(name)
-  puts "Hello, #{name}."
+  puts "Hi, my name is #{name}."
 end
 
 def introduction_with_language(name, language)
-  puts "Hello, #{name}, your language is #{language}."
+  puts "Hi, mt name is #{name} and I am learning to program in #{language}."
 end
